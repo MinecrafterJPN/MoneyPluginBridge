667d46190a2d10daf15708bc7a752606378d2e03
MinecrafterJPN <minecrafter.mn@gmail.com>
2014-12-21 10:38:18 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# MoneyPluginBridge
+
+Bridges among money plugins
+
+# Installation
+1.  Drop it into your /plugins folder.
+2.  Restart your server.
+
+# Outline
+
+This is a plugin for developers.
+This plugin bridges among money plugins.
+You can make your plugin without considering the differences among money plugins.
---
+++ src/MinecrafterJPN/PocketGuard.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace MinecrafterJPN;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+
+class MoneyPluginBridge extends PluginBase {
+    public function onLoad()
+	{
+	}
+
+	public function onEnable()
+	{
+    }
+
+	public function onDisable()
+	{
+	}
+
+	public function onCommand(CommandSender $sender, Command $command, $label, array $args)
+	{
+	}
+}
